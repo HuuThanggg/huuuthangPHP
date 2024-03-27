b5846d7c5041de909116b90b01e900ef6b383473
NguyenHuuThang <thangnguyen2662002@gmail.com>
2024-03-27 15:42:05 +0700

    code php

---
+++ header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="/KIEMTRA/site.css" rel="stylesheet">
+    <title>Quản lý nhân sự</title>
+</head>
+<body>
+    <header>
+        <div id="wrapper">
+            <h1>Quản Lý Nhân Sự</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Trang chủ</a></li>
+                    <li><a href="#">Giới thiệu</a></li>
+                    <li><a href="#">Dịch vụ</a></li>
+                    <li><a href="#">Liên hệ</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <main>
+        <div id="wrapper">
+            <h2>Chào mừng bạn đến với trang quản lý nhân sự</h2>
+            <p>Trang web này cung cấp các dịch vụ quản lý nhân sự hiệu quả cho doanh nghiệp của bạn.</p>
+            
+        </div>
+    </main>
+    
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php include_once("header.php"); ?>
+
+<ul class="menu">
+    <li>
+        <a href="/kiemtra/list_nhanvien.php">Danh sach nhan vien </a>
+    </li>
+    <li>
+        <a href="/kiemtra/add_nhanvien.php">Thêm nhân viên</a>
+    </li>
+
+</ul>
+
+
